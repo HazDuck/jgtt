a39305dddfd7b53a836da779ac77ff07dba8dfd1
Pete Heyes <peter.heyes@victorianplumbing.co.uk>
2024-07-14 11:59:48 +0100

    chore: Add initial project files and configurations

---
+++ assets/vite-app.js
@@ -0,0 +1,1 @@
+import{j as e,c as t,R as r}from"./vite-client.js";const o=()=>e.jsx("h1",{children:"AppLayer"});t.createRoot(document.getElementById("root")).render(e.jsx(r.StrictMode,{children:e.jsx(o,{})}));
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <h1>AppLayer</h1>;
+};
+
+export default App;
