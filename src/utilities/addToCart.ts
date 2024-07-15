87ff004fb05125af9f4655d23a1ce5ae9e287ee4
Pete Heyes <peter.heyes@victorianplumbing.co.uk>
2024-07-15 20:01:40 +0100

    Add try catch to atc

--- src/utilities/addToCart.ts
+++ src/utilities/addToCart.ts
@@ -4,32 +4,32 @@ const addToCart = async (variantId: number) => {
   const cartDrawer: any = document.querySelector("cart-drawer");
 
   const addToCartRequest = async () => {
-    const res = await fetch("/cart/add.js", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        items: [
-          {
-            id: variantId,
-            quantity: 1,
-          },
-        ],
-        sections: cartDrawer.getSectionsToRender().map((section: any) => {
-          return section.id;
+    try {
+      const res = await fetch("/cart/add.js", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          items: [
+            {
+              id: variantId,
+              quantity: 1,
+            },
+          ],
+          sections: cartDrawer.getSectionsToRender().map((section: any) => {
+            return section.id;
+          }),
         }),
-      }),
-    });
-    return res.json();
+      });
+      return res.json();
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const response = await addToCartRequest();
   cartDrawer.renderContents(response);
-
-  if (!response.ok) {
-    console.log(`something went wrong adding to cart`);
-  }
 };
 
 export default addToCart;
