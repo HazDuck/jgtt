b58e8ab62d584fc2617313c5a45993ddf2735010
Pete Heyes <peter.heyes@victorianplumbing.co.uk>
2024-07-15 21:25:09 +0100

    Remove return

--- src/components/molecules/ProductCard.tsx
+++ src/components/molecules/ProductCard.tsx
@@ -8,36 +8,34 @@ export const ProductCard = ({
 }: {
   product: any;
   buttonBackgroundColor: string;
-}) => {
-  return (
-    <li className="grid__item scroll-trigger animate--slide-in" key={id}>
-      <div className="card-wrapper product-card-wrapper underline-links-hover">
-        <div className="card card--standard card--media">
-          {/* @TODO: use srcset to get correct image size rather than always 400px */}
-          <img
-            src={`${media && media[0]?.src}&_width=400`}
-            alt={media && media[0]?.alt}
-            className="motion-reduce"
-            loading="lazy"
-          />
+}) => (
+  <li className="grid__item scroll-trigger animate--slide-in" key={id}>
+    <div className="card-wrapper product-card-wrapper underline-links-hover">
+      <div className="card card--standard card--media">
+        {/* @TODO: use srcset to get correct image size rather than always 400px */}
+        <img
+          src={`${media && media[0]?.src}&_width=400`}
+          alt={media && media[0]?.alt}
+          className="motion-reduce"
+          loading="lazy"
+        />
 
-          <div className="card__content">
+        <div className="card__content">
+          <div className="card__information">
+            <h3 className="card__heading h5">
+              <a href={`products/${handle}`}>{title}</a>
+            </h3>
             <div className="card__information">
-              <h3 className="card__heading h5">
-                <a href={`products/${handle}`}>{title}</a>
-              </h3>
-              <div className="card__information">
-                <span className="caption-large light"></span>
-                <Price price={price} />
-              </div>
+              <span className="caption-large light"></span>
+              <Price price={price} />
             </div>
           </div>
-          <AddToCartButton
-            buttonBackgroundColor={buttonBackgroundColor}
-            variantId={variants && variants[0]?.id}
-          />
         </div>
+        <AddToCartButton
+          buttonBackgroundColor={buttonBackgroundColor}
+          variantId={variants && variants[0] && variants[0]?.id}
+        />
       </div>
-    </li>
-  );
-};
+    </div>
+  </li>
+);
