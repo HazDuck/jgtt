c00bd92fdd649945f163f5f1111419d81cb6ecb7
Pete Heyes <peter.heyes@victorianplumbing.co.uk>
2024-07-15 08:08:07 +0100

    Add set recently viewed logic

---
+++ assets/product.js
@@ -0,0 +1,54 @@
+const addProductToLocalStorage = () => {
+  const jgRecentlyViewedData = localStorage.getItem("jgRecentlyViewedData");
+
+  //get product data from the DOM
+  const productData =
+    document.querySelector("[data-product-data").dataset.productData;
+  const { id, title, price, featured_image } = JSON.parse(productData);
+
+  const data = {
+    id,
+    title,
+    featured_image,
+    price,
+    time: Date.now(),
+  };
+
+  if (!jgRecentlyViewedData) {
+    localStorage.setItem(
+      "jgRecentlyViewedData",
+      JSON.stringify({ [id]: data })
+    );
+  } else {
+    const currentRVData = JSON.parse(
+      localStorage.getItem("jgRecentlyViewedData")
+    );
+
+    //make array of product data and check if current product is already in the array
+    const productData = Object.values(currentRVData);
+    const currentProductId = productData.find(
+      (item) => parseInt(item.id) === data.id
+    );
+
+    if (currentProductId) {
+      return;
+    } else {
+      const rvData = {};
+
+      // sort product data by time and get the last 3 items, turn back into obj and then add new item
+      productData.sort((a, b) => b.time - a.time);
+      productData.length = productData.length > 3 ? 3 : productData.length;
+      productData.forEach((item) => {
+        rvData[item.id] = item;
+      });
+      rvData[data.id] = data;
+
+      localStorage.setItem("jgRecentlyViewedData", JSON.stringify(rvData));
+    }
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM fully loaded and parsed");
+  addProductToLocalStorage();
+});
